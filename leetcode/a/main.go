498201ee2795b3933f9177f6149b8d26fc51e5d0
rema424 <rema.gapp@gmail.com>
2019-07-18 12:07:47 +0900

    wip: median

--- leetcode/a/main.go
+++ leetcode/a/main.go
@@ -7,5 +7,6 @@ import (
 )
 
 func main() {
-	fmt.Println(leetcode.LengthOfLongestSubstring("abcabcbb"))
+	// fmt.Println(leetcode.LengthOfLongestSubstring("abcabcbb"))
+	fmt.Println(leetcode.FindMedianSortedArrays_2([]int{1, 2, 3, 4, 5, 6, 7}, []int{1}))
 }
