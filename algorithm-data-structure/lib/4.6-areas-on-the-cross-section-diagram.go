d83226eb8d9c146ecdffecb8a0c0bd09780b5552
rema424 <rema.gapp@gmail.com>
2019-07-25 16:05:46 +0900

    add: area

--- algorithm-data-structure/lib/4.6-areas-on-the-cross-section-diagram.go
+++ algorithm-data-structure/lib/4.6-areas-on-the-cross-section-diagram.go
@@ -6,32 +6,81 @@ func ExecAreasOnTheCrossSectionDiagram() {
 	args := `\\///\_/\/\\\\/_/\\///__\\\_\\/_\/_/\`
 	runes := []rune(args)
 
-	stack := make([]int, 0, len(runes)+1)
-	top := 0
+	routeStack := newRouteStack(len(runes))
+	areaStack := newAreaStack(len(runes) / 2)
 
-	push := func(x int) {
-		top++
-		stack[top] = x
-	}
-
-	pop := func() int {
-		x := stack[top]
-		top--
-		return x
-	}
-
-	var area int
-	for i, r := range runes {
-		char := string(r)
-		fmt.Println(i, char)
-		switch char {
-		case `\`:
-			push(i)
-		case `/`:
-			x := pop()
-			area += i - x
-		default:
+	var char string
+	var areaSum int
+	for cur, r := range runes {
+		char = string(r)
+		if char == `\` {
+			routeStack.push(cur)
+		} else if char == `/` && routeStack.top > 0 {
+			left := routeStack.pop()
+			areaSum += cur - left
+			areaPart := cur - left
+			for {
+				if areaStack.top == 0 {
+					break
+				}
+				if areaStack.data[areaStack.top].left <= left {
+					break
+				}
+				old := areaStack.pop()
+				areaPart += old.area
+			}
+			areaStack.push(pair{left, areaPart})
 		}
 	}
-	fmt.Println(area)
+	fmt.Println(areaSum)
+	fmt.Print(areaStack.top, " ")
+	for i := 1; i <= areaStack.top; i++ {
+		fmt.Print(areaStack.data[i].area, " ")
+	}
+	fmt.Print("\n")
+}
+
+type routeStack struct {
+	top  int
+	data []int
+}
+
+func newRouteStack(size int) *routeStack {
+	return &routeStack{0, make([]int, size+1)}
+}
+
+func (s *routeStack) push(x int) {
+	s.top++
+	s.data[s.top] = x
+}
+
+func (s *routeStack) pop() int {
+	x := s.data[s.top]
+	s.top--
+	return x
+}
+
+type pair struct {
+	left int
+	area int
+}
+
+type areaStack struct {
+	top  int
+	data []pair
+}
+
+func newAreaStack(size int) *areaStack {
+	return &areaStack{0, make([]pair, size+1)}
+}
+
+func (s *areaStack) push(x pair) {
+	s.top++
+	s.data[s.top] = x
+}
+
+func (s *areaStack) pop() pair {
+	x := s.data[s.top]
+	s.top--
+	return x
 }
