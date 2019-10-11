89491b106be32f4f1d55107ca4de42987ff3a99e
rema424 <rema.gapp@gmail.com>
2019-10-11 13:15:25 +0900

    DBのラッパー

--- chat/infra/db.go
+++ chat/infra/db.go
@@ -62,8 +62,8 @@ type querier interface {
 	Query(query string, args ...interface{}) (*sql.Rows, error)
 }
 
-// ExDB ...
-type ExDB struct {
+// MyDB ...
+type MyDB struct {
 	querier   // 無名フィールド。クエリを発行する実体を入れる。具体的には sqlx.DB または sqlx.Tx
 	ctx       context.Context
 	WarnTime  time.Duration
@@ -71,10 +71,10 @@ type ExDB struct {
 	logHidden bool
 }
 
-// NewExDB ...
-func NewExDB(ctx context.Context, db *sqlx.DB) *ExDB {
-	return &ExDB{
-		querier:  db,
+// NewMyDB ...
+func NewMyDB(ctx context.Context, q querier) *MyDB {
+	return &MyDB{
+		querier:  q,
 		ctx:      ctx,
 		WarnTime: 150 * time.Millisecond,
 		WarnRows: 1000,
@@ -82,7 +82,7 @@ func NewExDB(ctx context.Context, db *sqlx.DB) *ExDB {
 }
 
 // Get ...
-func (db *ExDB) Get(dest interface{}, query string, args ...interface{}) error {
+func (db *MyDB) Get(dest interface{}, query string, args ...interface{}) error {
 	start := time.Now()
 	err := db.querier.Get(dest, query, args...)
 	db.log(start, err, query, args, countRows(dest))
@@ -90,10 +90,10 @@ func (db *ExDB) Get(dest interface{}, query string, args ...interface{}) error {
 }
 
 // TxFunc ...
-type TxFunc func(db *ExDB) error
+type TxFunc func(db *MyDB) error
 
-// ExecTx ...
-func (db *ExDB) ExecTx(fn TxFunc) error {
+// BeginTx ...
+func (db *MyDB) BeginTx(fn TxFunc) error {
 	// トランザクション開始済み（querierの実体が*sqlx.Tx）ならそのままクエリ実行
 	if _, ok := db.querier.(*sqlx.Tx); ok {
 		return fn(db)
@@ -102,26 +102,89 @@ func (db *ExDB) ExecTx(fn TxFunc) error {
 	// トランザクション開始前（querierの実体が*sqlx.DB）ならトランザクションを開始
 	sqlxDB, ok := db.querier.(*sqlx.DB)
 	if !ok {
-		return fmt.Errorf("invalid ExDB")
+		return fmt.Errorf("invalid MyDB")
 	}
 
-	tx, err := sqlxDB.Begin()
+	tx, err := sqlxDB.Beginx()
 	if err != nil {
 		return err
 	}
 
-	newExDB := &ExDB{
-		querier:   tx,
-		ctx:       db.ctx,
-		WarnTime:  db.WarnTime,
-		WarnRows:  db.WarnRows,
-		logHidden: db.logHidden,
-	}
-
-	return execTx
+	return db.execTxFn(tx, fn)
 }
 
-func (db *ExDB) log(start time.Time, err error, query string, args []interface{}, count int) {
+func (db *MyDB) execTxFn(tx *sqlx.Tx, fn TxFunc) (err error) {
+	defer func() {
+		// recover() で panic を捕捉して制御を分ける
+		if r := recover(); r != nil {
+			// panic が発生したらロールバックを実行
+			if rberr := tx.Rollback(); rberr != nil {
+				// ロールバックエラーがある場合はログ出力
+				if rberr == sql.ErrTxDone {
+					log.Printf("debug: rollback error %s", rberr)
+				} else {
+					log.Printf("error: rollback error %s", rberr)
+				}
+			}
+
+			// panic の内容をエラーとして返却
+			if rerr, ok := r.(error); ok {
+				err = rerr
+			} else {
+				err = fmt.Errorf("%v", r)
+			}
+		} else if err != nil {
+			// panic が発生せず、error が発生した場合はロールバックを実行
+			if rberr := tx.Rollback(); rberr != nil {
+				// ロールバックエラーがある場合はログ出力
+				if rberr == sql.ErrTxDone {
+					log.Printf("debug: rollback error %s", rberr)
+				} else {
+					log.Printf("error: rollback error %s", rberr)
+				}
+			}
+		} else {
+			// panic も error も発生していない場合はコミットを実行
+			if cmerr := tx.Commit(); cmerr != nil {
+				if cmerr != sql.ErrTxDone {
+					err = cmerr
+				}
+			}
+		}
+	}()
+
+	txMyDB := &MyDB{
+		querier:  tx, // *sqlx.DB ではなく *sqlx.Tx を利用してクエリを実行する
+		ctx:      db.ctx,
+		WarnTime: db.WarnTime,
+		WarnRows: db.WarnRows,
+	}
+
+	err = fn(txMyDB)
+
+	return
+}
+
+func (db *MyDB) execTxFnLess(tx *sqlx.Tx, fn TxFunc) (err error) {
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+		} else {
+			tx.Commit()
+		}
+	}()
+
+	txMyDB := &MyDB{
+		querier: tx, // *sqlx.DB ではなく *sqlx.Tx を利用してクエリを実行する
+		ctx:     db.ctx,
+	}
+
+	err = fn(txMyDB)
+
+	return
+}
+
+func (db *MyDB) log(start time.Time, err error, query string, args []interface{}, count int) {
 
 }
 
