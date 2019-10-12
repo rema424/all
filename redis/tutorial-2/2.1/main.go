1cb6fc16b2152173068638b820f28ecf29a412d7
rema424 <rema.gapp@gmail.com>
2019-10-12 20:22:35 +0900

    init 2.2

--- redis/tutorial-2/2.1/main.go
+++ redis/tutorial-2/2.1/main.go
@@ -27,7 +27,7 @@ func main() {
 	flag.Parse()
 
 	r := newRoom()
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/room", r)
 
 	// チャットルームを開始します
