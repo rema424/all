feb4bac94fe862af459514e3b7427c8fdd169b7f
rema424 <rema.gapp@gmail.com>
2019-10-12 20:24:05 +0900

    init 2.2

---
+++ redis/tutorial-2/2.2/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	addr := flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse()
+
+	r := newRoom()
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/room", r)
+
+	// チャットルームを開始します
+	go r.run()
+
+	// Webサーバーを起動します
+	log.Println("Webサーバーを開始します。ポート：", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
