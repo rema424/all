7f94683aa270287eed66eb1ddca1c4a29ab2402f
rema424 <rema.gapp@gmail.com>
2019-10-21 18:17:12 +0900

    gcsサンプル

---
+++ learning/google-cloud-storage/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"cloud.google.com/go/storage"
+	"google.golang.org/api/iterator"
+	"google.golang.org/api/option"
+)
+
+func main() {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx, option.WithCredentialsFile("./service_account_key.json"))
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println("Buckets:")
+	it := client.Buckets(ctx, "michael-sandbox7")
+	for {
+		battrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		fmt.Println(battrs.Name)
+	}
+}
