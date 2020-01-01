23677a6d934fe74a7ebba1a3f7136e50014d7d70
rema424 <rema.gapp@gmail.com>
2020-01-01 17:43:45 +0900

    validation

--- web/chi/go.mod
+++ web/chi/go.mod
@@ -5,7 +5,7 @@ go 1.13
 require (
 	github.com/flosch/pongo2 v0.0.0-20190707114632-bbf5a6c351f4
 	github.com/go-chi/chi v4.0.2+incompatible
-	github.com/go-chi/render v1.0.1
+	github.com/go-playground/validator/v10 v10.1.0
 	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
 	github.com/k0kubun/pp v3.0.1+incompatible
 	github.com/mattn/go-colorable v0.1.4 // indirect
--- web/chi/main.go
+++ web/chi/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -12,11 +11,13 @@ import (
 	"github.com/flosch/pongo2"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-playground/validator/v10"
 	"github.com/k0kubun/pp"
 )
 
 var (
 	renderer = NewRenderer("web/template")
+	validate = validator.New()
 )
 
 func main() {
@@ -28,47 +29,9 @@ func main() {
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Heartbeat("/ping"))
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-type", "text/html")
-		renderer.Execute(w, "top.html", nil)
-	})
-
-	r.Get("/signup", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-type", "text/html")
-		renderer.Execute(w, "auth/signup.html", nil)
-	})
-
-	r.Post("/signup", func(w http.ResponseWriter, r *http.Request) {
-		// pp.Println(r)
-		in := struct {
-			Email    string `json:"email"`
-			Password string `json:"password"`
-		}{
-			Email:    r.FormValue("email"),
-			Password: r.FormValue("password"),
-		}
-
-		pp.Println(in)
-		w.Header().Add("Content-type", "text/html")
-		renderer.Execute(w, "auth/signup.html", nil)
-	})
-
-	r.Get("/login", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-type", "text/html")
-		renderer.Execute(w, "auth/login.html", nil)
-	})
-
-	r.Post("/login", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-type", "text/html")
-		renderer.Execute(w, "auth/login.html", nil)
-	})
-
-	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-type", "application/json")
-		json.NewEncoder(w).Encode(map[string]interface{}{
-			"status": "ok",
-		})
-	})
+	r.Get("/", HandleGetTop)
+	r.Get("/signup", HandleGetSignup)
+	r.Post("/signup", HandlePostSignup)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -104,10 +67,80 @@ func (r *Renderer) Execute(w io.Writer, name string, data map[string]interface{}
 }
 
 // ------------------------------
-// SignupForm
+// Validate
+// ------------------------------
+
+type ValidationError struct {
+	Field   string `json:"field"`
+	Tag     string `json:"tag"`
+	Message string `json:"message"`
+}
+
+// ------------------------------
+// Top
+// ------------------------------
+
+func HandleGetTop(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-type", "text/html")
+	renderer.Execute(w, "top.html", nil)
+}
+
+// ------------------------------
+// Signup
 // ------------------------------
 
 type SignupForm struct {
-	Email    string `validate:"-"`
-	Password string `validate:"-"`
+	Email    string `validate:"required,email"`
+	Password string `validate:"required,min=8,max=15"`
+}
+
+var SignupFormFields = map[string]string{
+	"Email":    "メールアドレス",
+	"Password": "パスワード",
+}
+
+func parseSignupFormError(err error) []ValidationError {
+	errs := make([]ValidationError, 0, 2)
+	for _, err := range err.(validator.ValidationErrors) {
+		f := err.Field()
+		t := err.Tag()
+		var msg string
+		switch t {
+		case "required":
+			msg = fmt.Sprintf("%sの入力は必須です。", SignupFormFields[f])
+		case "email":
+			msg = fmt.Sprintf("%sの形式が不正です。", SignupFormFields[f])
+		default:
+			switch f {
+			case "Password":
+				if t == "min" || t == "max" {
+					msg = fmt.Sprintf("%sは8文字以上15文字以内で入力してください。", SignupFormFields[f])
+				}
+			}
+		}
+		if msg != "" {
+			errs = append(errs, ValidationError{f, t, msg})
+		}
+	}
+	return errs
+}
+
+func HandleGetSignup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-type", "text/html")
+	renderer.Execute(w, "auth/signup.html", nil)
+}
+
+func HandlePostSignup(w http.ResponseWriter, r *http.Request) {
+	in := SignupForm{
+		Email:    r.FormValue("email"),
+		Password: r.FormValue("password"),
+	}
+
+	if err := validate.Struct(in); err != nil {
+		log.Println(parseSignupFormError(err))
+	}
+
+	pp.Println(in)
+	w.Header().Add("Content-type", "text/html")
+	renderer.Execute(w, "auth/signup.html", nil)
 }
