bf6c67099306a722c90d7973fb242467673a4358
rema424 <rema.gapp@gmail.com>
2020-01-01 16:51:08 +0900

    chi

---
+++ web/chi/go.mod
@@ -0,0 +1,13 @@
+module chi
+
+go 1.13
+
+require (
+	github.com/flosch/pongo2 v0.0.0-20190707114632-bbf5a6c351f4
+	github.com/go-chi/chi v4.0.2+incompatible
+	github.com/go-chi/render v1.0.1
+	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
+	github.com/k0kubun/pp v3.0.1+incompatible
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
+)
--- web/chi/main.go
+++ web/chi/main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-	"chappie/renderer"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 
+	"github.com/flosch/pongo2"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/k0kubun/pp"
 )
 
 var (
-	ren = renderer.New("web/template")
+	renderer = NewRenderer("web/template")
 )
 
 func main() {
@@ -21,11 +24,45 @@ func main() {
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
 	r.Use(middleware.RealIP)
+	r.Use(middleware.DefaultCompress)
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Heartbeat("/ping"))
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-type", "text/html")
+		renderer.Execute(w, "top.html", nil)
 	})
+
+	r.Get("/signup", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-type", "text/html")
+		renderer.Execute(w, "auth/signup.html", nil)
+	})
+
+	r.Post("/signup", func(w http.ResponseWriter, r *http.Request) {
+		// pp.Println(r)
+		in := struct {
+			Email    string `json:"email"`
+			Password string `json:"password"`
+		}{
+			Email:    r.FormValue("email"),
+			Password: r.FormValue("password"),
+		}
+
+		pp.Println(in)
+		w.Header().Add("Content-type", "text/html")
+		renderer.Execute(w, "auth/signup.html", nil)
+	})
+
+	r.Get("/login", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-type", "text/html")
+		renderer.Execute(w, "auth/login.html", nil)
+	})
+
+	r.Post("/login", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-type", "text/html")
+		renderer.Execute(w, "auth/login.html", nil)
+	})
+
 	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-type", "application/json")
 		json.NewEncoder(w).Encode(map[string]interface{}{
@@ -35,7 +72,7 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
+		port = "3333"
 		log.Printf("Defaulting to port %s", port)
 	}
 	log.Printf("Listening on port %s", port)
@@ -43,4 +80,34 @@ func main() {
 	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), r); err != nil {
 		log.Fatal(err)
 	}
+}
+
+// ------------------------------
+// Renderer
+// ------------------------------
+
+func NewRenderer(tmplDir string) *Renderer {
+	return &Renderer{tmplDir}
+}
+
+type Renderer struct {
+	TmplDir string
+}
+
+func (r *Renderer) Execute(w io.Writer, name string, data map[string]interface{}) error {
+	b, err := pongo2.Must(pongo2.FromCache(filepath.Join(r.TmplDir, name))).ExecuteBytes(data)
+	if err != nil {
+		return err
+	}
+	_, err = w.Write(b)
+	return err
+}
+
+// ------------------------------
+// SignupForm
+// ------------------------------
+
+type SignupForm struct {
+	Email    string `validate:"-"`
+	Password string `validate:"-"`
 }
