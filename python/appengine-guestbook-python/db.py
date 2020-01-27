1e109b908cbb4026beac58a5ae383d830605fabc
rema424 <rema.gapp@gmail.com>
2020-01-27 15:12:29 +0900

    add sqlalchemy

--- python/appengine-guestbook-python/db.py
+++ python/appengine-guestbook-python/db.py
@@ -5,12 +5,33 @@ conn = sa.create_engine(
     encoding='utf8',
     echo=True)
 
-res = conn.execute('''
-CREATE TABLE IF NOT EXISTS zoo (
-    critter VARCHAR(255) PRIMARY KEY,
-    count INT,
-    damages FLOAT
-);
-''')
+# res = conn.execute('''
+# CREATE TABLE IF NOT EXISTS zoo (
+#     critter VARCHAR(255) PRIMARY KEY,
+#     count INT,
+#     damages FLOAT
+# );
+# ''')
+# print(res)
 
-print(res)
+# ins = 'INSERT IGNORE INTO zoo (critter, count, damages) VALUES (%s, %s, %s);'
+# conn.execute(ins, 'duck', 10, 0.0)
+# conn.execute(ins, 'bear', 2, 1000.0)
+# conn.execute(ins, 'weasel', 1, 2000.0)
+# rows = conn.execute('SELECT * FROM zoo')
+# for row in rows:
+#     print(row)
+
+meta = sa.MetaData()
+zoo = sa.Table('zoo', meta,
+               sa.Column('critter', sa.String(255), primary_key=True),
+               sa.Column('count', sa.Integer),
+               sa.Column('damages', sa.Float),
+               )
+meta.create_all(conn)
+conn.execute(zoo.insert(('bear', 2, 1000.0)))
+conn.execute(zoo.insert(('weasel', 1, 2000.0)))
+conn.execute(zoo.insert(('duck', 10, 0)))
+result = conn.execute(zoo.select())
+rows = result.fetchall()
+print(rows)
