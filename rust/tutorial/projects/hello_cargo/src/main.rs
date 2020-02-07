ce5dfa001d262ef77e71445a252e094ac3864cde
rema424 <rema.gapp@gmail.com>
2020-02-07 15:46:36 +0900

    hello rust

---
+++ rust/tutorial/projects/hello_cargo/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+}
---
+++ rust/tutorial/projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/rm/dev/all/rust/tutorial/projects/hello_cargo/target/debug/hello_cargo: /Users/rm/dev/all/rust/tutorial/projects/hello_cargo/src/main.rs
