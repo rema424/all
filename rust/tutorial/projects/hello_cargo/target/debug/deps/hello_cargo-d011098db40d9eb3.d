a67d5285b6a3e4bcd552bf0ae14509d5d8aab01d
rema424 <rema.gapp@gmail.com>
2020-04-30 14:10:49 +0900

    add

--- rust/tutorial/projects/hello_cargo/target/debug/deps/hello_cargo-d011098db40d9eb3.d
+++
@@ -1,5 +0,0 @@
-/Users/rm/dev/all/rust/tutorial/projects/hello_cargo/target/debug/deps/hello_cargo-d011098db40d9eb3: src/main.rs
-
-/Users/rm/dev/all/rust/tutorial/projects/hello_cargo/target/debug/deps/hello_cargo-d011098db40d9eb3.d: src/main.rs
-
-src/main.rs:
--- rust/tutorial/projects/hello_cargo/target/debug/hello_cargo.d
+++ rust/tutorial/projects/hello_cargo/target/debug/hello_cargo.d
@@ -1,1 +1,1 @@
-/Users/rm/dev/all/rust/tutorial/projects/hello_cargo/target/debug/hello_cargo: /Users/rm/dev/all/rust/tutorial/projects/hello_cargo/src/main.rs
+/Users/rema/dev/all/rust/tutorial/projects/hello_cargo/target/debug/hello_cargo: /Users/rema/dev/all/rust/tutorial/projects/hello_cargo/src/main.rs
