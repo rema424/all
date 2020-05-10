f2c3d1094519b1ad2252cfa9a79cc3fb43098271
rema424 <rema.gapp@gmail.com>
2020-05-10 18:41:13 +0900

    add

--- python/deep-learning-from-scratch-2/ch02/simple_cbow.py
+++ python/deep-learning-from-scratch-2/ch02/simple_cbow.py
@@ -2,6 +2,7 @@
 import sys
 sys.path.append('..')
 import numpy as np
+from ch02.simple_skip_gram import SimpleSkipGram
 from common.layers import MatMul, SoftmaxWithLoss
 from common.trainer import Trainer
 from common.optimizer import SGD, Adam
@@ -14,6 +15,15 @@ class SimpleCBOW:
         # 重みの初期化
         W_in = 0.01 * np.random.randn(V, H).astype('f')
         W_out = 0.01 * np.random.randn(H, V).astype('f')
+        W_in = np.array([
+            [-1.0655735,  1.3231287, -1.1051644, -1.1049938, -1.0685176],
+            [1.1559865,  0.08719956, 1.1672966,  1.1607609,  1.1567391],
+            [-0.7532327,   0.6444376,  -0.76896185, -0.71775854, -0.7918966],
+            [0.9111972,  1.9940354,  0.6837302,  0.89859486, 0.87255],
+            [-0.78328615,  0.6444221,  -0.7729693,  -0.7400077,  -0.80646306],
+            [-1.058986,   1.3268483, -1.1123687, -1.1059289, -1.0616288],
+            [ 1.1203294, -1.6394324,  1.2104743, 1.1509397,  1.1612827]
+        ]).astype('f')
 
         # レイヤの生成
         self.in_layer0 = MatMul(W_in)
@@ -61,7 +71,8 @@ if __name__ == "__main__":
     target = convert_one_hot(target, vocab_size)
     contexts = convert_one_hot(contexts, vocab_size)
 
-    model = SimpleCBOW(vocab_size, hidden_size)
+    # model = SimpleCBOW(vocab_size, hidden_size)
+    model = SimpleSkipGram(vocab_size, hidden_size)
     # optimizer = SGD()
     optimizer = Adam()
     trainer = Trainer(model, optimizer)
@@ -69,3 +80,6 @@ if __name__ == "__main__":
     trainer.fit(contexts, target, max_epoch, batch_size)
     trainer.plot()
 
+    word_vecs = model.word_vecs
+    for word_id, word in id_to_word.items():
+        print(word, word_vecs[word_id])
